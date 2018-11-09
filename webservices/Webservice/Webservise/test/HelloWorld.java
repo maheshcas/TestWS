003b489c42860e025abb119b95b7f26455d127ce
maheshcas <mahesh.cas@gmail.com>
2018-11-09 11:37:40 +0530

    Added New Test file

---
+++ webservices/Webservice/Webservise/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package Webservice.Webservise.test;
+
+
+
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
+}
